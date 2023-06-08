be973965ddcb4da3b17d8843bb205e24cf55f2c2
theprakharsaxena <prakharsaxena5125@gmail.com>
2023-06-08 19:26:16 +0530

    update footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
 import { HiShoppingCart } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const renderImages = (repeatCount) => {
+    const images = [];
+
+    for (let i = 0; i < repeatCount; i++) {
+      images.push(
+        <img
+          key={i}
+          src="/images/star.png"
+          alt={`Image ${i}`}
+          className="w-6"
+        />
+      );
+    }
+
+    return images;
+  };
   return (
     <>
       <div className="bg-[#f8f6f3] px-20 py-20 grid grid-cols-2 gap-20 justify-center items-center">
@@ -101,7 +118,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="flex justify-center items-center flex-col">
+      <div className="bg-[#30536612] px-28 py-20 grid grid-cols-1 gap-16 justify-center items-center">
         <div className="flex flex-col items-center space-y-3">
           <h2 className="text-3xl mb-3">Top Categories</h2>
           <img
@@ -114,7 +131,7 @@ const Home = () => {
             }}
           />
         </div>
-        <div className="bg-[#30536612] py-20 px-28 w-screen grid grid-cols-3 gap-20">
+        <div className="grid grid-cols-3 gap-20">
           <div className="bg-white p-10 flex flex-col justify-between items-center space-y-64">
             <h3 className="text-3xl">Handmade</h3>
             <button className="flex bg-[#305366] hover:bg-green-700 justify-center space-x-2 items-center w-6/12 p-2 rounded">
@@ -188,14 +205,101 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="flex justify-center items-center py-20 flex-col space-y-5">
-        <div className="flex flex-col items-center space-y-3">
+      <div className="flex justify-center items-center py-20 flex-col space-y-10">
+        <div className="flex flex-col items-center space-y-1">
           <h2 className="text-3xl">Testimonials</h2>
-          <img
-            src="/images/review1.png"
-            alt="Review"
-            className="w-16"
-          />
+          <img src="/images/review1.png" alt="Review" className="w-16" />
+        </div>
+        <div className="flex justify-center space-x-20">
+          <div className="bg-[#D9D9D9] p-10 flex flex-col justify-between items-center space-y-40 rounded-3xl min-w-[33%] mt-16">
+            <div className="flex space-x-1">{renderImages(4)}</div>
+            <h4 className="text-xl">Prakhar Saxena</h4>
+          </div>
+          <div className="bg-[#D9D9D9] p-10 flex flex-col justify-between items-center space-y-40 rounded-3xl min-w-[33%] mb-16">
+            <div className="flex space-x-1">{renderImages(5)}</div>
+            <h4 className="text-xl">Ankit Yadav</h4>
+          </div>
+          <div className="bg-[#D9D9D9] p-10 flex flex-col justify-between items-center space-y-40 rounded-3xl min-w-[33%] mt-16">
+            <div className="flex space-x-1">{renderImages(4)}</div>
+            <h4 className="text-xl">Prakhar Saxena</h4>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#305366D6] p-10 grid grid-cols-4 text-white">
+        <div></div>
+        <div className="flex flex-col items-center space-y-3">
+          <h3 className="text-2xl">Quick Links</h3>
+          <Link className="font-normal" to="/">
+            Home
+          </Link>
+          <Link className="font-normal" to="/about">
+            About Us
+          </Link>
+          <Link className="font-normal" to="/services">
+            Services
+          </Link>
+          <Link className="font-normal" to="/shop">
+            Shop
+          </Link>
+          <Link className="font-normal" to="/contact">
+            Contact
+          </Link>
+        </div>
+        <div className="flex flex-col items-center space-y-3">
+          <h3 className="text-2xl">Site Links</h3>
+          <Link className="font-normal" to="/about">
+            Return Policy
+          </Link>
+          <Link className="font-normal" to="/services">
+            Terms & Conditions
+          </Link>
+          <Link className="font-normal" to="/shop">
+            Privacy Policy
+          </Link>
+          <Link className="font-normal" to="/contact">
+            Shipping
+          </Link>
+        </div>
+        <div className="flex flex-col items-center space-y-5">
+          <h3 className="text-2xl">Social</h3>
+          <div className="flex space-x-5">
+            <Link>
+              <img
+                src="/images/instagramicon.png"
+                alt="Instagram"
+                className="w-7"
+              />
+            </Link>
+            <Link>
+              <img
+                src="/images/facebookicon.png"
+                alt="Facebook"
+                className="w-7"
+              />
+            </Link>
+            <Link>
+              <img
+                src="/images/linkedinicon.png"
+                alt="LinkedIn"
+                className="w-7"
+              />
+            </Link>
+            <Link>
+              <img
+                src="/images/twittericon.png"
+                alt="Twitter"
+                className="w-7"
+              />
+            </Link>
+          </div>
+          <div className="flex space-x-2">
+            <img
+              src="/images/playstore.png"
+              alt="Play Store"
+              className="w-32"
+            />
+            <img src="/images/appstore.png" alt="App Store" className="w-32" />
+          </div>
         </div>
       </div>
     </>
