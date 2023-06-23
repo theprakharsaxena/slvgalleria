dbdd034d1d62f7de4df5dddbd63fd228f45210ae
theprakharsaxena <prakharsaxena5125@gmail.com>
2023-06-23 22:32:56 +0530

    update code

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -66,8 +66,8 @@ const Shop = () => {
       </Helmet>
       <div>
         <div className="p-11 bg-black"></div>
-        <section className="my-12 px-[6%]">
-          <div className="flex justify-between items-center mb-[4%]">
+        <section className="py-3 px-[1%] bg-gray-200">
+          {/* <div className="flex justify-between items-center mb-[3%]">
             <h2 className="text-4xl font-serif">New Designs</h2>
             <Link
               to="/shop"
@@ -75,49 +75,112 @@ const Shop = () => {
             >
               Explore More
             </Link>
-          </div>
-          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 sm:gap-7 gap-12">
-            {data.map(({ image, title, creator, featured, price }, index) => {
-              return (
-                <div className="flex flex-col space-y-5" key={index}>
-                  <div className="bg-pink-200 border border-pink-900 p-10 hover:bg-pink-900 relative">
-                    <div className="h-[350px] w-auto max-h-[350px] flex items-center justify-center">
-                      <img
-                        src={`/images/${image}`}
-                        alt={title}
-                        className="h-[350px]"
-                      />
-                    </div>
-                    <div className="absolute bottom-0 right-0 text-pink-900 flex flex-col items-center p-1 space-y-2">
-                      <Link>
-                        <AiFillHeart className="text-4xl p-1 rounded bg-pink-200 hover:bg-white" />
-                      </Link>
-                      <Link>
-                        <FaEye className="text-[36px] p-1 rounded bg-pink-200 hover:bg-white" />
-                      </Link>
-                      <Link>
-                        <BiCartAdd className="text-4xl p-1 rounded bg-pink-200 hover:bg-white" />
-                      </Link>
-                    </div>
-                  </div>
-                  <div>
-                    <h3 className="text-xl mb-1">{title}</h3>
-                    <div className="flex space-x-2 font-serif">
-                      <h4 className="text-pink-600">Creator:</h4>
-                      <p>{creator}</p>
-                    </div>
-                    <div className="flex space-x-2 font-serif">
-                      <h4 className="text-pink-600">Featured in:</h4>
-                      <p>{featured}</p>
-                    </div>
-                    <div className="flex space-x-2 font-serif">
-                      <h4 className="text-pink-600">Price:</h4>
-                      <span>&#8377; {price}</span>
-                    </div>
+          </div> */}
+          <div className="grid grid-cols-5 gap-3">
+            <div className="flex flex-col border h-min bg-white">
+              <h3 className="p-4 text-2xl font-bold font-serif">Filters</h3>
+              <div className="border"></div>
+              <div className="p-4">
+                <h4 className="text-xl">Categories</h4>
+                <div className="text-gray-500 p-2">
+                  <h5>Current Category</h5>
+                </div>
+                <ul className="py-3 flex flex-col space-y-1.5">
+                  <li className="flex items-center space-x-2">
+                    <input type="checkbox" />
+                    <p>Category 1</p>
+                  </li>
+                  <li className="flex items-center space-x-2">
+                    <input type="checkbox" />
+                    <p>Category 2</p>
+                  </li>
+                  <li className="flex items-center space-x-2">
+                    <input type="checkbox" />
+                    <p>Category 3</p>
+                  </li>
+                  <li className="flex items-center space-x-2">
+                    <input type="checkbox" />
+                    <p>Category 4</p>
+                  </li>
+                  <li className="flex items-center space-x-2">
+                    <input type="checkbox" />
+                    <p>Category 5</p>
+                  </li>
+                  <li className="flex items-center space-x-2">
+                    <input type="checkbox" />
+                    <p>Category 6</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-span-4 bg-white border grid grid-cols-1">
+              <div>
+                <div className="text-sm px-4 pt-4 pb-1 flex flex-col space-y-2">
+                  <h3 className="text-xl font-bold font-serif">
+                    Showing result for "abstract painting"
+                  </h3>
+                  <div className="flex space-x-5">
+                    <h4 className="font-bold">Sort By</h4>
+                    <ul className="flex space-x-4">
+                      <li className="hover:underline underline-offset-8 hover:text-pink-900 cursor-pointer">Relevance</li>
+                      <li className="hover:underline underline-offset-8 hover:text-pink-900 cursor-pointer">Popularity</li>
+                      <li className="hover:underline underline-offset-8 hover:text-pink-900 cursor-pointer">Price -- Low to High</li>
+                      <li className="hover:underline underline-offset-8 hover:text-pink-900 cursor-pointer">Price -- High to Low</li>
+                      <li className="hover:underline underline-offset-8 hover:text-pink-900 cursor-pointer">Newest First</li>
+                    </ul>
                   </div>
                 </div>
-              );
-            })}
+                <div className="border"></div>
+              </div>
+              <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 p-4">
+                {data.map(
+                  ({ image, title, creator, featured, price }, index) => {
+                    return (
+                      <div
+                        className="flex flex-col space-y-5 mb-10 p-1 hover:p-0 cursor-pointer"
+                        key={index}
+                      >
+                        <div className="bg-pink-50 border border-pink-200 p-5 hover:bg-pink-200 relative">
+                          <div className="h-[250px] w-auto flex items-center justify-center">
+                            <img
+                              src={`/images/${image}`}
+                              alt={title}
+                              className="h-[250px]"
+                            />
+                          </div>
+                          <div className="absolute bottom-0 right-0 flex flex-col text-pink-900 items-center p-0.5 space-y-2">
+                            <Link>
+                              <AiFillHeart className="text-3xl p-1 rounded bg-black/20 hover:bg-white" />
+                            </Link>
+                            <Link>
+                              <FaEye className="text-[30px] p-1 rounded bg-black/20 hover:bg-white" />
+                            </Link>
+                            <Link>
+                              <BiCartAdd className="text-3xl p-1 rounded bg-black/20 hover:bg-white" />
+                            </Link>
+                          </div>
+                        </div>
+                        <div>
+                          <h3 className="text-xl mb-1">{title}</h3>
+                          <div className="flex space-x-2 font-serif">
+                            <h4 className="text-pink-600">Creator:</h4>
+                            <p>{creator}</p>
+                          </div>
+                          <div className="flex space-x-2 font-serif">
+                            <h4 className="text-pink-600">Featured in:</h4>
+                            <p>{featured}</p>
+                          </div>
+                          <div className="flex space-x-2 font-serif">
+                            <h4 className="text-pink-600">Price:</h4>
+                            <span>&#8377; {price}</span>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  }
+                )}
+              </div>
+            </div>
           </div>
         </section>
       </div>
